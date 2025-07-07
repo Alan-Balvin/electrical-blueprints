42f79cdbe29005b77a85594d78a4d2fe4a7b2235
Alan-Balvin <tareauasbalvin@gmail.com>
2025-07-07 10:01:56 -0700

    updating navbar features and location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import blueprints from '@/data/blueprints.json';
 import BlueprintSearch from './ui/BlueprintsSearch';
-import Navbar from './ui/Navbar';
+
 import { useSession, signIn } from 'next-auth/react';
 
 const S3_FOLDER = process.env.NEXT_PUBLIC_S3_FOLDER;
@@ -15,7 +15,7 @@ export default function Home() {
     return (
       <main className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-gray-100 to-gray-300 p-6">
         <h1 className="text-5xl font-extrabold text-center text-gray-800 mb-6">
-          Welcome to Electrical Blueprints
+          Restricted Access to Electrical Blueprints
         </h1>
         <p className="text-lg text-gray-600 mb-8 text-center max-w-md">
           Access detailed electrical plans for Novotel Living Mazatlán. Please sign in with GitHub to continue.
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
     <>
-      <Navbar />
+      
       <main className="h-[calc(100vh-64px)] p-8 bg-gradient-to-br from-gray-50 to-gray-200 flex max-w-7xl mx-auto gap-6">
         <aside className="w-72 flex-shrink-0 border-r border-gray-300 pr-4 flex flex-col">
           <h2 className="text-xl font-semibold mb-4 text-gray-800">Plan List</h2>
