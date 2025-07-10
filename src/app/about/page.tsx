027daad37e6cdb21d31c96e9edc07b0a9844d4d9
Alan-Balvin <tareauasbalvin@gmail.com>
2025-07-10 10:52:30 -0700

    configuration of background images for about and contact pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,13 @@ export default function AboutPage() {
     <div className="relative min-h-screen bg-cover bg-center">
       <div className="absolute inset-0 bg-white bg-opacity-70 backdrop-blur-sm" />
 <div
-          className="absolute inset-0 bg-center bg-cover opacity-40 filter brightness-110 contrast-125 pointer-events-none z-0"
+          className="absolute inset-0 bg-center bg-cover opacity-70 filter brightness-110 contrast-125 pointer-events-none z-0"
           style={{ backgroundImage: "url('/images/background2.png')" }}
         />
       <main className="relative p-8 max-w-3xl mx-auto z-10">
         
-        <h1 className="text-4xl font-bold mb-6">About</h1>
+       <h1 className="text-4xl font-bold mb-6">About</h1>
+
         <p className="text-lg text-gray-700">
           This project was built by Pablo Cancino using Next.js 15 and TailwindCSS.
           It allows users to view and search electrical blueprints for Novotel Living Mazatlán.
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,13 +2,13 @@ export default function ContactPage() {
   return (
     <div className="relative min-h-screen bg-cover bg-center">
       <div className="absolute inset-0 bg-white bg-opacity-70 backdrop-blur-sm" />
-
-      <main className="relative p-8 max-w-3xl mx-auto z-10">
-        <div
-          className="absolute inset-0 bg-center bg-cover opacity-40 filter brightness-110 contrast-125 pointer-events-none z-0"
+<div
+          className="absolute inset-0 bg-center bg-cover opacity-70 filter brightness-110 contrast-125 pointer-events-none z-0"
           style={{ backgroundImage: "url('/images/background3.png')" }}
         />
-        <h1 className="text-4xl font-bold mb-6">Contact</h1>
+      <main className="relative p-8 max-w-3xl mx-auto z-10">
+        
+        <h1 className="text-4xl font-bold mb-6 ">Contact</h1>
         <p className="text-lg text-gray-700">
           Feel free to contact me at:
           <a
